05cdf4121aed1d9c61039b8172bf6eb9c495d02b
Harrota <dansirua@gmail.com>
2020-05-20 23:06:14 +0300

    added password encryption

--- src/main/java/com/harrota/controller/RegistrationController.java
+++ src/main/java/com/harrota/controller/RegistrationController.java
@@ -3,18 +3,23 @@ package com.harrota.controller;
 import com.harrota.model.Role;
 import com.harrota.model.User;
 import com.harrota.repos.UserRepo;
+import com.harrota.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
+
 
     @GetMapping("/registration")
     public String registration() {
@@ -23,22 +28,20 @@ public class RegistrationController {
 
 
     @PostMapping("/registration")
-    public String addUser(User user, Map<String, Object> model) {
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-        User userFromDbByEmail = userRepo.findByEmail(user.getEmail());
-        if (userFromDbByEmail != null) {
-            model.put("message", "Email is taken!");
+    public String addUser(@Valid User user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
+
+            model.mergeAttributes(errors);
+
             return "registration";
         }
 
-        if (userFromDb != null) {
-            model.put("message", "User with this username already exists!");
+        if (!userService.addUser(user)) {
+            model.addAttribute("usernameError", "User exists!");
             return "registration";
         }
-
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
 
         return "redirect:/login";
     }
--- src/main/java/com/harrota/service/UserService.java
+++ src/main/java/com/harrota/service/UserService.java
@@ -1,19 +1,56 @@
 package com.harrota.service;
 
+import com.harrota.model.Role;
+import com.harrota.model.User;
 import com.harrota.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepo.findByEmail(email);
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return user;
     }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+
+        if (userFromDb != null) {
+            return false;
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        userRepo.save(user);
+
+        return true;
+    }
+
+    public List<User> findAll() {
+        return (List<User>) userRepo.findAll();
+    }
+
 }
